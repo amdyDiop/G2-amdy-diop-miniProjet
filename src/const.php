35fd746c027363334222c5180f25694509540072
dofbi <amdymila@gmail,com>
2020-04-10 12:45:41 +0000

    US1: done

---
+++ src/const.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> mini projet php </title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="miniProjet.css">
+</head>
+<?php
+
+?>
+
+    <body>
+        <div class="global">
+            <div class="header">
+                <img class="logo" src="Images/logo-QuizzSA.png" alt="logo quiz">
+                Le plaisir de jouer
+            </div>
+            <div class="content">
+          
+            
+            </div>
+        </div>
+
+
+    </body>
+
+</html>
