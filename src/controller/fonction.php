f5f0f00352f79a4b27b067cf1de07e3c13ca674d
dofbi <amdymila@gmail,com>
2020-04-20 15:04:46 +0000

    Us5 done

--- src/controller/fonction.php
+++ src/controller/fonction.php
@@ -45,4 +45,21 @@ function getRole($login,$password){
     }
 }
 
+function existeLogin($login){
+$files = '../../../assets/json/user.json';
+$db = file_get_contents($files);
+$db = json_decode($db, true);
+$teste = 0;
+for ($i = 0; $i < count($db); $i++) {
+    if (strcmp($db[$i]['login'], $login) == 0) {
+        $teste = 1;
+        break;
+    }
+}
+if ($teste == 0)
+    return 1;
+return 0;
+
+}
+
 ?>
